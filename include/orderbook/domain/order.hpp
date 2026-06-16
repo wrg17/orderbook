3bc35c5904de5d4d5359ae043e354c437dace859
William George <wrg@umich.edu>
2026-06-15 23:45:11 -0400

    Mark Order interface noexcept; abort on formatter throw

--- include/orderbook/domain/order.hpp
+++ include/orderbook/domain/order.hpp
@@ -13,7 +13,7 @@
 
 struct OrderId {
     std::uint64_t value;
-    constexpr std::strong_ordering operator<=>(const OrderId&) const = default;
+    constexpr std::strong_ordering operator<=>(const OrderId&) const noexcept = default;
 };
 
 enum class Side : std::uint8_t { BUY, SELL };
@@ -21,60 +21,60 @@ enum class Side : std::uint8_t { BUY, SELL };
 struct Quantity {
     std::uint32_t value;
 
-    constexpr std::strong_ordering operator<=>(const Quantity&) const = default;
+    constexpr std::strong_ordering operator<=>(const Quantity&) const noexcept = default;
 
-    constexpr Quantity& operator-=(Quantity other) {
+    constexpr Quantity& operator-=(Quantity other) noexcept {
         value -= other.value;
         return *this;
     }
 
-    constexpr Quantity& operator+=(Quantity other) {
+    constexpr Quantity& operator+=(Quantity other) noexcept {
         value += other.value;
         return *this;
     }
 
-    constexpr Quantity& operator*=(std::uint32_t scalar) {
+    constexpr Quantity& operator*=(std::uint32_t scalar) noexcept {
         value *= scalar;
         return *this;
     }
 };
 
-constexpr Quantity operator-(Quantity lhs, Quantity rhs) {
+constexpr Quantity operator-(Quantity lhs, Quantity rhs) noexcept {
     return lhs -= rhs;
 }
 
-constexpr Quantity operator+(Quantity lhs, Quantity rhs) {
+constexpr Quantity operator+(Quantity lhs, Quantity rhs) noexcept {
     return lhs += rhs;
 }
 
-constexpr Quantity operator*(Quantity lhs, std::uint32_t scalar) {
+constexpr Quantity operator*(Quantity lhs, std::uint32_t scalar) noexcept {
     return lhs *= scalar;
 }
 
-constexpr Quantity operator*(std::uint32_t scalar, Quantity rhs) {
+constexpr Quantity operator*(std::uint32_t scalar, Quantity rhs) noexcept {
     return rhs *= scalar;
 }
 
 struct Ticker {
     std::uint16_t value;
-    constexpr std::strong_ordering operator<=>(const Ticker&) const = default;
+    constexpr std::strong_ordering operator<=>(const Ticker&) const noexcept = default;
 };
 
 // Stored in 1/1000 of a currency unit (1000 == $1.00).
 struct Price {
     std::uint64_t value;
-    constexpr std::strong_ordering operator<=>(const Price&) const = default;
+    constexpr std::strong_ordering operator<=>(const Price&) const noexcept = default;
 };
 
 class Order {
 public:
-    Order(Side side, Quantity quantity, Ticker ticker, Price price);
+    Order(Side side, Quantity quantity, Ticker ticker, Price price) noexcept;
 
-    [[nodiscard]] OrderId getId() const;
-    [[nodiscard]] Side getSide() const;
-    [[nodiscard]] Quantity getQuantity() const;
-    [[nodiscard]] Ticker getTicker() const;
-    [[nodiscard]] Price getPrice() const;
+    [[nodiscard]] OrderId getId() const noexcept;
+    [[nodiscard]] Side getSide() const noexcept;
+    [[nodiscard]] Quantity getQuantity() const noexcept;
+    [[nodiscard]] Ticker getTicker() const noexcept;
+    [[nodiscard]] Price getPrice() const noexcept;
 
 private:
     inline static std::atomic<std::uint64_t> id_seq = 0;
@@ -86,10 +86,10 @@ private:
     Price price_;
 };
 
-std::ostream& operator<<(std::ostream& os, const Order& order);
+std::ostream& operator<<(std::ostream& os, const Order& order) noexcept;
 
 template <> struct std::formatter<OrderId> : std::formatter<std::uint64_t> {
-    std::format_context::iterator format(OrderId id, std::format_context& ctx) const;
+    std::format_context::iterator format(OrderId id, std::format_context& ctx) const noexcept;
 };
 
 template <> struct std::hash<OrderId> {
@@ -99,19 +99,20 @@ template <> struct std::hash<OrderId> {
 };
 
 template <> struct std::formatter<Side> : std::formatter<std::string_view> {
-    std::format_context::iterator format(Side s, std::format_context& ctx) const;
+    std::format_context::iterator format(Side s, std::format_context& ctx) const noexcept;
 };
 
 template <> struct std::formatter<Quantity> : std::formatter<std::uint32_t> {
-    std::format_context::iterator format(Quantity quantity, std::format_context& ctx) const;
+    std::format_context::iterator format(Quantity quantity,
+                                         std::format_context& ctx) const noexcept;
 };
 
 template <> struct std::formatter<Ticker> : std::formatter<std::uint16_t> {
-    std::format_context::iterator format(Ticker ticker, std::format_context& ctx) const;
+    std::format_context::iterator format(Ticker ticker, std::format_context& ctx) const noexcept;
 };
 
 template <> struct std::formatter<Price> : std::formatter<std::uint64_t> {
-    std::format_context::iterator format(Price price, std::format_context& ctx) const;
+    std::format_context::iterator format(Price price, std::format_context& ctx) const noexcept;
 };
 
 template <> struct std::formatter<Order> {
@@ -119,5 +120,6 @@ template <> struct std::formatter<Order> {
     parse(const std::format_parse_context& ctx) {
         return ctx.begin();
     }
-    static std::format_context::iterator format(const Order& order, std::format_context& ctx);
+    static std::format_context::iterator format(const Order& order,
+                                                std::format_context& ctx) noexcept;
 };
--- src/domain/order.cpp
+++ src/domain/order.cpp
@@ -2,63 +2,93 @@
 // Created by Will George on 3/22/26.
 //
 
+#include <cstdlib>
 #include <format>
 #include <orderbook/domain/order.hpp>
 
-Order::Order(Side side, Quantity quantity, Ticker ticker, Price price)
+Order::Order(Side side, Quantity quantity, Ticker ticker, Price price) noexcept
     : id_(OrderId{++id_seq}), side_(side), quantity_(quantity), ticker_(ticker), price_(price) {}
 
-OrderId Order::getId() const {
+OrderId Order::getId() const noexcept {
     return id_;
 }
 
-Side Order::getSide() const {
+Side Order::getSide() const noexcept {
     return side_;
 }
 
-Quantity Order::getQuantity() const {
+Quantity Order::getQuantity() const noexcept {
     return quantity_;
 }
 
-Ticker Order::getTicker() const {
+Ticker Order::getTicker() const noexcept {
     return ticker_;
 }
 
-Price Order::getPrice() const {
+Price Order::getPrice() const noexcept {
     return price_;
 }
 
-std::ostream& operator<<(std::ostream& os, const Order& order) {
-    return os << std::format("{}", order);
+std::ostream& operator<<(std::ostream& os, const Order& order) noexcept {
+    try {
+        return os << std::format("{}", order);
+    } catch (...) {
+        std::abort();
+    }
 }
 
-std::format_context::iterator std::formatter<OrderId>::format(OrderId id,
-                                                              std::format_context& ctx) const {
-    return std::formatter<std::uint64_t>::format(id.value, ctx);
+std::format_context::iterator
+std::formatter<OrderId>::format(OrderId id, std::format_context& ctx) const noexcept {
+    try {
+        return std::formatter<std::uint64_t>::format(id.value, ctx);
+    } catch (...) {
+        std::abort();
+    }
 }
 
-std::format_context::iterator std::formatter<Side>::format(Side s, std::format_context& ctx) const {
-    return std::formatter<std::string_view>::format(s == Side::BUY ? "BUY" : "SELL", ctx);
+std::format_context::iterator
+std::formatter<Side>::format(Side s, std::format_context& ctx) const noexcept {
+    try {
+        return std::formatter<std::string_view>::format(s == Side::BUY ? "BUY" : "SELL", ctx);
+    } catch (...) {
+        std::abort();
+    }
 }
 
-std::format_context::iterator std::formatter<Quantity>::format(Quantity quantity,
-                                                               std::format_context& ctx) const {
-    return std::formatter<std::uint32_t>::format(quantity.value, ctx);
+std::format_context::iterator
+std::formatter<Quantity>::format(Quantity quantity, std::format_context& ctx) const noexcept {
+    try {
+        return std::formatter<std::uint32_t>::format(quantity.value, ctx);
+    } catch (...) {
+        std::abort();
+    }
 }
 
-std::format_context::iterator std::formatter<Ticker>::format(Ticker ticker,
-                                                             std::format_context& ctx) const {
-    return std::formatter<std::uint16_t>::format(ticker.value, ctx);
+std::format_context::iterator
+std::formatter<Ticker>::format(Ticker ticker, std::format_context& ctx) const noexcept {
+    try {
+        return std::formatter<std::uint16_t>::format(ticker.value, ctx);
+    } catch (...) {
+        std::abort();
+    }
 }
 
-std::format_context::iterator std::formatter<Price>::format(Price price,
-                                                            std::format_context& ctx) const {
-    return std::formatter<std::uint64_t>::format(price.value, ctx);
+std::format_context::iterator
+std::formatter<Price>::format(Price price, std::format_context& ctx) const noexcept {
+    try {
+        return std::formatter<std::uint64_t>::format(price.value, ctx);
+    } catch (...) {
+        std::abort();
+    }
 }
 
 std::format_context::iterator std::formatter<Order>::format(const Order& order,
-                                                            std::format_context& ctx) {
-    return std::format_to(ctx.out(), "Order(Id:{},Side:{},Quantity:{},Ticker:{},Price:{})",
-                          order.getId(), order.getSide(), order.getQuantity(), order.getTicker(),
-                          order.getPrice());
+                                                            std::format_context& ctx) noexcept {
+    try {
+        return std::format_to(ctx.out(), "Order(Id:{},Side:{},Quantity:{},Ticker:{},Price:{})",
+                              order.getId(), order.getSide(), order.getQuantity(),
+                              order.getTicker(), order.getPrice());
+    } catch (...) {
+        std::abort();
+    }
 }
